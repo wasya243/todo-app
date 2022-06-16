6ffd80db8f8dfb5564b697ca22c4d077dccf286f
Vasyl <vasyl@voicett.com>
2022-06-16 23:08:40 +0300

    Try fix delpoyment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/test-db',
     SERVER: {
-        PORT: process.env.SERVER_PORT || 5000
+        PORT: process.env.PORT || 5000
     },
     NODE_ENV: process.env.NODE_ENV || 'local'
 }
